c2de478e212d733567f22954fc0c8a20568c5da6
Hassaan Fayyaz Ahmed <hassaanfayyaz@gmail.com>
2019-07-27 13:19:20 +0500

    Added refresh control.

--- Mooncascade-Test/ViewControllers/EmployeeListViewController.swift
+++ Mooncascade-Test/ViewControllers/EmployeeListViewController.swift
@@ -14,28 +14,48 @@ class EmployeeListViewController: UIViewController, UITableViewDelegate, UITable
     @IBOutlet weak var employeeTableView: UITableView!
     
     var employeeList: [Employee]?
-    
+
+    private let refreshControl = UIRefreshControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.employeeTableView.tableFooterView = UIView.init()
+        self.employeeTableView.refreshControl = refreshControl
+        // Configure Refresh Control
+        refreshControl.addTarget(self, action: #selector(refreshEmployeeList(_:)), for: .valueChanged)
+        refreshControl.attributedTitle = NSAttributedString.init(string: "Loading Employees")
+
+        self.fetchEmployeeList()
+    }
+    
+    // MARK: API Requests
+    
+    /// Fetch Latest employee list from server
+    private func fetchEmployeeList() {
         
         self.activityIndicator.startAnimating()
         NetworkManager.sharedInstance.getHomePage { [weak self] (empArr, errStr) in
             DispatchQueue.main.async {
                 self?.activityIndicator.stopAnimating()
-                
+                self?.refreshControl.endRefreshing()
+
                 if let errStr = errStr {
                     print(errStr)
                 } else {
                     self?.employeeList = empArr
                     self?.employeeTableView.reloadData()
-                    print(empArr)
                 }
             }
         }
     }
 
+    // MARK: Refresh Control
+    @objc private func refreshEmployeeList(_ sender: Any) {
+        // Fetch Weather Data
+        self.fetchEmployeeList()
+    }
+    
     // MARK: UITableview
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -51,7 +71,6 @@ class EmployeeListViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
         
     }
     
